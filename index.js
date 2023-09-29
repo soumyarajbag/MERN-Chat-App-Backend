84974132a58e71bc619d076c5d87c02295ef925b
soumyarajbag <soumyarajbag@gmail.com>
2023-09-30 03:34:31 +0530

    Usercontroller updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const colors = require("colors");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const connectDB = require("./config/db");
@@ -12,6 +14,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 app.listen(5000, () => {
   console.log("SERVER STARTED !".yellow.bold);
 });
